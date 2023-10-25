050e1b678826c3b1f37e16953bb0572da6c13210
vogel <over.castrillon22396@ucaldas.edu.co>
2023-10-24 21:06:57 -0500

    Role class addition.

---
+++ models/roles.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database.db import Base
+
+
+class Rol(Base):
+
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100))
+    description = Column(String(255))
+
+    users = relationship("User", back_populates="rol")
