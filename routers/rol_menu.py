c2587ae342f7c07dd5e313e9710380b4d31d4b02
vogel <over.castrillon22396@ucaldas.edu.co>
2023-10-25 20:34:41 -0500

    Now it's functional the reading, creation and deleting or roles and menus.

--- routers/rol_menu.py
+++ routers/rol_menu.py
@@ -6,19 +6,24 @@ from services.rol_menu import add_rol_menu, remove_rol_menu, get_menus_role
 
 router = APIRouter()
 
+
 @router.post("/rol/{rol_id}/menu/{menu_id}")
-async def add_menu_to_rol(rol_id: int, menu_id: int, db: Session = Depends(get_db)):
-    if await add_rol_menu(db, rol_id, menu_id):
+def add_menu_to_rol(rol_id: int, menu_id: int, db: Session = Depends(get_db)):
+    if add_rol_menu(db, rol_id, menu_id):
         return {"message": "Menu added to Rol successfully"}
     raise HTTPException(status_code=400, detail="Operation failed")
 
+
 @router.get("/menus/{rol_id}")
-async def get_menus_by_role(rol_id: int, db: Session = Depends(get_db)):
-    return await get_menus_role(db, rol_id)
+def get_menus_by_role(rol_id: int, db: Session = Depends(get_db)):
+    menus = get_menus_role(db, rol_id)
+    if menus:
+        return menus
+    raise HTTPException(status_code=404, detail="Menus not found")
+
 
 @router.delete("/rol/{rol_id}/menu/{menu_id}")
-async def remove_menu_from_rol(rol_id: int, menu_id: int, db: Session = Depends(get_db)):
-    if await remove_rol_menu(db, rol_id, menu_id):
+def remove_menu_from_rol(rol_id: int, menu_id: int, db: Session = Depends(get_db)):
+    if remove_rol_menu(db, rol_id, menu_id):
         return {"message": "Menu removed from Rol successfully"}
     raise HTTPException(status_code=400, detail="Operation failed")
-
