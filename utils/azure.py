1462018b667e47d6572584722b63c7c37a014e61
vogel <over.castrillon22396@ucaldas.edu.co>
2023-11-15 17:47:48 -0500

    Last blob changes.

--- utils/azure.py
+++ utils/azure.py
@@ -4,10 +4,14 @@ from azure.storage.blob import BlobServiceClient
 
 
 # Configura la conexión a Azure Blob Storage
-connection_string = "DefaultEndpointsProtocol=https;AccountName=estremorstorageaccount;AccountKey=/mCPhntPNfJg04502cvRTNd1ZooObPUYWl9hC3fWv60h8bl/NHCfC8yTkGQkH7Z6PMIcmFgdrnQd+AStIQSUMA==;EndpointSuffix=core.windows.net"
-container_name = "estremoruserscontainer"
+connection_string = 'DefaultEndpointsProtocol=https;AccountName=imgblobstorage;AccountKey=yK62qR/KVNHLgIM+1BL5NZ+1oAHVQMDvmr/SoCL2DjPgoBU0CT/qfXFvAYAaIp45t6QsPy8ae+wS+AStwVsgdQ==;EndpointSuffix=core.windows.net'
 
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+container_name = 'hh-user-images'
+
+
+blob_service_client = BlobServiceClient.from_connection_string(
+    connection_string
+)
 container_client = blob_service_client.get_container_client(container_name)
 
 
@@ -21,10 +25,8 @@ def upload_file_to_azurecontainer(file: UploadFile, filename: str):
         with file.file as f:
             blob_client.upload_blob(f, overwrite=True)
           # Devuelve la URL de la imagen
-        image_url = f"https://{blob_client.account_name}.blob.core.windows.net/{container_name}/{filename}"
+        image_url = f'https://{blob_client.account_name}.blob.core.windows.net/{
+            container_name}/{filename}'
         return image_url
     except Exception as e:
-        return {"error": str(e)}
-    
-
-    
+        return {'error': str(e)}
