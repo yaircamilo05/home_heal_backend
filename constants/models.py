3a65041f8c5bd3a66beba87db30c4ccd8d490f23
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2023-11-15 19:20:23 -0500

    Update default user image URL and load Azure connection string from environment variable.

--- constants/models.py
+++ constants/models.py
@@ -1,1 +1,1 @@
-DEFAULT_IMG = 'https://estremorstorageaccount.blob.core.windows.net/estremoruserscontainer/usuario_318-804790.png'
+DEFAULT_IMG = 'https://imgblobstorage.blob.core.windows.net/hh-user-images/user_deffault.jpeg'
--- utils/azure.py
+++ utils/azure.py
@@ -1,19 +1,21 @@
+import os
+from dotenv import load_dotenv
 from fastapi import UploadFile
 from fastapi import UploadFile
 from azure.storage.blob import BlobServiceClient
 
+env = os.getenv("ENV","development")
+path = f'.env.{env}'
+dotenv_path = os.path.join('environments', path)
+load_dotenv(dotenv_path=dotenv_path)
 
+CONECTION_STRING = os.getenv("CONTAINER_IMAGES_AZURE_CONECTION_STRING")
 # Configura la conexión a Azure Blob Storage
-connection_string = 'DefaultEndpointsProtocol=https;AccountName=imgblobstorage;AccountKey=yK62qR/KVNHLgIM+1BL5NZ+1oAHVQMDvmr/SoCL2DjPgoBU0CT/qfXFvAYAaIp45t6QsPy8ae+wS+AStwVsgdQ==;EndpointSuffix=core.windows.net'
-
+connection_string = CONECTION_STRING
 container_name = 'hh-user-images'
 
-
-blob_service_client = BlobServiceClient.from_connection_string(
-    connection_string
-)
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 container_client = blob_service_client.get_container_client(container_name)
-
 
 def upload_file_to_azurecontainer(file: UploadFile, filename: str):
     if file is None:
@@ -25,8 +27,7 @@ def upload_file_to_azurecontainer(file: UploadFile, filename: str):
         with file.file as f:
             blob_client.upload_blob(f, overwrite=True)
           # Devuelve la URL de la imagen
-        image_url = f'https://{blob_client.account_name}.blob.core.windows.net/{
-            container_name}/{filename}'
+        image_url = f'https://{blob_client.account_name}.blob.core.windows.net/{container_name}/{filename}'
         return image_url
     except Exception as e:
         return {'error': str(e)}
