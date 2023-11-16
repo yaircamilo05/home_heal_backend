38c2e6d8bbb3b41cfee8f040ba8ec0c03881ee2f
julianagomez21 <jgomezgal25@gmail.com>
2023-11-16 17:38:30 -0500

    Add UserRegister parsing to register_patient_user

--- routers/patient.py
+++ routers/patient.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends, Form, UploadFile, status, File
+import json
+from fastapi import APIRouter, Body, Depends, Form, UploadFile, status, File
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from pydantic import parse_obj_as
 from requests import Session
 from database.db import get_db
 
@@ -10,8 +12,12 @@ from services.patient import all_patients, register_user
 
 router = APIRouter()
 
+def parse_user(user: str = Body(...)):
+    return parse_obj_as(UserRegister, json.loads(user))
+
 @router.post("/register_user")
-def register_patient_user(user: UserRegister = Depends(UserRegister), image_file: UploadFile = File(...), db: Session = Depends(get_db)):
+def register_patient_user(user: UserRegister = Depends(parse_user), image_file: UploadFile = File(...), db: Session = Depends(get_db)):
+    print('estoy aqui')
     new_patient = register_user(user,image_file, db)
     if new_patient == None:
          return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": "Error al registrar el usuario"})
