eead35455be773fd65ea479567779952d1f330ae
Davidrc26 <david.ramirez22171@ucaldas.edu.co>
2023-11-20 21:28:18 -0500

    crud user changes

--- schemas/user.py
+++ schemas/user.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from fastapi import UploadFile
 from schemas.menu import MenuOut
+from schemas.rol import RolOut
 from typing import List, Optional
 from constants.models import DEFAULT_IMG
 
@@ -18,16 +19,35 @@ class User(BaseModel):
 
 class UserCreate(User):
     password: str
+    specialty: str
 
 
 class UserWithMenus(MenuOut):
     menus: List[MenuOut] = []
 
 
-class UserGet(User):
+class UserGet(BaseModel):
     id: int
     name: str
     lastname: str
     email: str
     image_url: str
+    rol: RolOut
+    cc: str
+    phone: str
+    specialty: str
+
+
+class UserAux(BaseModel):
+    name: str
+    lastname: str
+    email: str
+    image_url: str
     rol_id: int
+    cc: str
+    phone: str
+    password: str
+
+
+class UserUpdate(User):
+    specialty: str
--- services/user.py
+++ services/user.py
@@ -1,26 +1,54 @@
-from models.base import User
-from schemas.user import UserCreate, UserGet
+from models.base import User, Doctor
+from schemas.user import UserCreate, UserGet, UserAux, UserUpdate
+from schemas.doctor import Doctor as DoctorSchema
 from schemas.user import User as UserSchema
-from models.base import User
+from services.doctor import create_doctor, get_doctor_by_user_id, put_doctor
+from schemas.rol import RolOut
+from models.base import User, Rol
 from utils import auth
 from constants.models import DEFAULT_IMG
+from sqlalchemy import inspect
+from sqlalchemy.orm import joinedload
+from sqlalchemy import select
 
 def create_user(new_user: UserCreate, db):
+    print('llegue a crear usuario')
     exist = exist_user(new_user.email, db)
     if exist:
         return None
-    user = User(**new_user.model_dump())
+    
+    user_aux = UserAux(**new_user.__dict__)
+    
+    user = User(**user_aux.model_dump())
+  
+   
     if user.image_url == '' or user.image_url is None:
         user.image_url = DEFAULT_IMG
 
     # Encriptation of the password
     user.password = auth.encript_password(user.password)
     ## Acá va la logica de consulta en la base de datos
+
+    
     db.add(user)
     db.commit()
     db.refresh(user)
-    return user
+    
+    if new_user.specialty != '':
+        print('Entre a la creacion del doctor')
+        
+        print(user.id)
+        doctor = DoctorSchema(speciality=new_user.specialty, user_id=user.id)
+        
+        doctor_bd=create_doctor(doctor, db)
+        if doctor_bd is None:
+            db.delete(user)
+            db.commit()
+            return None
+    return instance_to_dict(user)
 
+def instance_to_dict(instance):
+    return {c.key: getattr(instance, c.key) for c in inspect(instance).mapper.column_attrs}
 
 def exist_user(email: str, db):
     print(email)
@@ -30,26 +58,47 @@ def exist_user(email: str, db):
 
 def all_users(db) -> list[UserGet]:
     result = []
-    users = db.query(User).all();
+    users = db.query(User).options(joinedload(User.rol)).all()
+    speciality = ''
+    
     for user in users:
-        result.append(UserGet(**user.__dict__))
+        try:
+            doctor:Doctor = get_doctor_by_user_id(user.id,db)
+            if doctor is not None:
+                speciality = doctor.speciality
+            result.append(UserGet(**instance_to_dict(user), rol=RolOut(**instance_to_dict(user.rol)), specialty=speciality))
+        except Exception as e:
+            print(e)
+            continue
     return result
 
 
-def put_user(id: int, user: User, db):
+def put_user(id: int, user: UserCreate, db):
+    print('entre a la modificacion del usuario')
     db_user: User = db.query(User).filter(User.id == id).first()
+    medico: Doctor = db.query(Doctor).filter(Doctor.user_id == id).first()
+    if (medico is not None):
+        print('entre a la modificacion del doctor')
+        doc= put_doctor(medico.id, DoctorSchema(speciality=user.specialty, user_id=db_user.id), db)
+        if doc is None:
+            return None  
     if db_user is None:
         return None
-    
+    ##user_aux = UserSchema(**user.model_dump())
     for attr, value in user.model_dump().items():
+        print('Entre a la modificacion del usuario(cilo)')
         setattr(db_user, attr, value)
     db.commit()
     db.refresh(db_user)
-    return db_user
+    return instance_to_dict(db_user)
 
 
 def delete_user(id: int, db):
     db_user: User = db.query(User).filter(User.id == id).first()
+    medico: Doctor = db.query(Doctor).filter(Doctor.user_id == id).first()
+    if (medico is not None):
+        db.delete(medico)
+        db.commit()
     if db_user is None:
         return None
     db.delete(db_user)
