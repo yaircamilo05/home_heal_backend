3467979e9ffbc8d4126f4479ad909894462f5b1c
julianagomez21 <jgomezgal25@gmail.com>
2023-11-23 21:15:15 -0500

    Add email functionality for sending vital signs

--- routers/email.py
+++ routers/email.py
@@ -1,20 +1,27 @@
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
-from schemas.email import EmailData, EmailRegisterData
-from services.email import send_email_register, send_email_doctor
+from schemas.email import EmailData, EmailRegisterData, EmailVitalSignsData
+from services.email import send_email_register, send_email_doctor_admin, send_email_vital_signs
 
 
 router = APIRouter()
 @router.post("/send_email_register")
-def send_register(data: EmailRegisterData):
+def send_register_email(data: EmailRegisterData):
     response = send_email_register(data)
     if response == "ko":
          return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": "Error al enviar el email"})
     return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"message": "Email enviado correctamente"})
         
 @router.post("/send_email_doctor_admin")
-def send_register_doctor_admin(data: EmailRegisterData):
-    response = send_email_doctor(data)
+def send_register_doctor_admin_email(data: EmailRegisterData):
+    response = send_email_doctor_admin(data)
+    if response == "ko":
+         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": "Error al enviar el email"})
+    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"message": "Email enviado correctamente"})
+
+@router.post("/send_email_vital_signs")
+def send_vital_signs_email(data: EmailVitalSignsData):
+    response = send_email_vital_signs(data)
     if response == "ko":
          return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": "Error al enviar el email"})
     return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"message": "Email enviado correctamente"})
