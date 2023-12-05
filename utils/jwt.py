350000fb52201adaca58342192fee6f3b03adbee
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2023-12-04 23:44:17 -0500

    Fix URL in change password endpoint

--- utils/jwt.py
+++ utils/jwt.py
@@ -1,48 +1,53 @@
 import datetime
 import os
 import time
-#from jwt import encode, decode
-# from jwt import DecodeError, ExpiredSignatureError
-from schemas.user import User, UserGetLogin, UserWithMenus
+from jose import jwt
+from schemas.user import UserGetLogin
 
 def create_token(user: UserGetLogin) -> str:
-    # key_hash = os.environ.get('KEY_HASH_TOKEN')
-    # token: str = encode(
-    #     payload=user.model_dump(),
-    #     key=key_hash, algorithm='HS256'
-    # )
-    # return token
-    return user.model_dump()
+    key_hash = os.environ.get('KEY_HASH_TOKEN')
+    token: str = jwt.encode(
+        claims=user.model_dump(),
+        key=key_hash, 
+        algorithm='HS256'
+    )
+    return token
 
 def create_token_email(user: UserGetLogin) -> str:
     payload = user.model_dump()
     key_hash = os.environ.get('KEY_HASH_TOKEN')
-    payload['exp'] = int(time.mktime((datetime.datetime.now() + datetime.timedelta(minutes=5)).timetuple()))
-    # token: str = encode(
-    #     payload=payload,
-    #     key=key_hash, 
-    #     algorithm='HS256'
-    # )
-    return payload
+    payload['exp'] = int(time.mktime((datetime.datetime.now() + datetime.timedelta(minutes=3)).timetuple()))
+    token: str = jwt.encode(
+        claims=payload,
+        key=key_hash, 
+        algorithm='HS256'
+    )
+    return token
 
 def validate_token(token: str) -> dict:
     key_hash = os.environ.get('KEY_HASH_TOKEN')
-    # try:
-    #     # data: dict = decode(token, key_hash, algorithms=['HS256'])
-    #     return token
-    # except DecodeError:
-    #     return None
-    # except ExpiredSignatureError:
-    #     return None
-    return token
+    try:
+        data: dict = jwt.decode(
+            token=token, 
+            key=key_hash, 
+            algorithms=['HS256']
+        )
+        return data
+    except jwt.JWTError:
+        return None
+    except jwt.ExpiredSignatureError:
+        return None
 
 def validate_token_email(token: str):
-    # key_hash = os.environ.get('KEY_HASH_TOKEN')
-    # try:
-    #     # data: dict = decode(token, key_hash, algorithms=['HS256'])
-    #     return token
-    # except DecodeError:
-    #     return -1
-    # except ExpiredSignatureError:
-    #     return -2
-    return token
+    key_hash = os.environ.get('KEY_HASH_TOKEN')
+    try:
+        data: dict = jwt.decode(
+            token=token, 
+            key=key_hash, 
+            algorithms=['HS256']
+        )
+        return data
+    except jwt.JWTError:
+        return -1
+    except jwt.ExpiredSignatureError:
+        return -2
